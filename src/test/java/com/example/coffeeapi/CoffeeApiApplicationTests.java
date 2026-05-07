cb4c3701992030cb08caa7e5f59890b12622bc1f
eunjiom <norgg021@gmail.com>
2026-05-07 16:48:13 +0900

    feat: 패키지 추가

---
+++ src/test/java/com/example/coffeeapi/CoffeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
